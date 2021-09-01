edbbe5517c5255832b5ae5fc4a301f9c8533b36c
LerssiUkko <89898085+LerssiUkko@users.noreply.github.com>
2021-09-01 13:32:58 +0300

    Update selectmenu.js

--- commands/selectmenu.js
+++ commands/selectmenu.js
@@ -13,27 +13,41 @@ module.exports = {
 				new MessageSelectMenu()
 					.setCustomId('ebinkkona')
 					.setPlaceholder('Nothing selected')
-					.setMinValues(2)
-					.setMaxValues(3)
+					.setMinValues(1)
+					.setMaxValues(5)
 					.addOptions([
 						{
 							label: 'Joku paska',
 							description: 'Tämä on paska',
-							value: 'vittu',
+							value: 'paska',
 						},
 						{
 							label: 'Joku vittu',
 							description: 'Tämä on vittu',
-							value: 'paska',
+							value: 'vittu',
 						},
 						{
+							label: 'kkonaaa',
+							description: 'kkonas',
+							value: 'kkona',
+						},
+                        {
 							label: 'Vittu eihän tää paska varmaan edes toimi',
 							description: 'ääääääää',
 							value: 'paska_ei_toimi_gg',
 						},
+                        {
+							label: 'mutsis',
+							description: 'vittu',
+							value: 'mutsis',
+						},
+                        {
+							label: 'kkong adn the kkona',
+							description: 'ö',
+							value: 'kkong',
+						},
 					]),
 			);
-
             await interaction.reply({ content: 'Bliugeli blau', components: [row] });
 
 	},
